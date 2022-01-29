345fa23e05e5e532b4c079159d11b391389c2b52
Pankaj Lagu <pankaz@users.noreply.github.com>
2022-01-30 01:16:02 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from setuptools import setup
 
 setup(
     name='obsidian-html',
-    version='0.1',
+    version='0.2',
     description='Converts an Obsidian vault into HTML',
-    url='https://github.com/kmaasrud/obsidian-hugo',
-    author='kmaasrud',
-    author_email='kmaasrud@outlook.com',
+    url='https://github.com/pankaz/obsidian-html',
+    author='pankaz',
+    author_email='pankaj.lagu@gmail.com',
     license='MIT',
     packages=['obsidian_html'],
     install_requires=[
